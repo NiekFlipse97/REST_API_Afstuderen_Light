e43b37d65563da8730a5d7395e6955a94d69151f
Niek Flipse <npaflips@avans.nl>
2020-03-24 22:27:09 +0100

    added dataAccess files

---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const UserRepository = require('../dataAccess/userRepository');
+
+router.get('/', (req, res) => {
+
+});
---
+++ schemas/UserSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const PostSchema = require('./PostSchema');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type:String,
+        required: [true, 'Email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    dob: {
+        type: Date,
+        required: [true, 'Date of birth is required']
+    },
+    friends: [this.UserSchema],
+    posts: [PostSchema.PostSchema]
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = {User, UserSchema};
