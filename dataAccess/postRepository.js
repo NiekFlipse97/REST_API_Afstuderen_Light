3e78e6d4976f8d31e67121ac77e432c4407bf5b1
Niek Flipse <npaflips@avans.nl>
2020-03-25 16:44:31 +0100

    Update postRepository.js

--- dataAccess/postRepository.js
+++ dataAccess/postRepository.js
@@ -4,11 +4,11 @@ const ApiErrors = require('../errorMessages/apiErrors');
 class PostRepository {
     static createPost(title, description, upvotes, downvotes) {
         const internalServerError = ApiErrors.internalServerError();
-        return new Promise((resolve, reject) => {
-            Post.findOne({title, description})
-                .then(post => {
-
-            })
-        })
+        // return new Promise((resolve, reject) => {
+        //     Post.findOne({title, description})
+        //         .then(post => {
+        //
+        //     })
+        // })
     }
 }
